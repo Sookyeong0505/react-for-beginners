3744578a26d206972e8b14f235f746ce91c7be28
sookyeong <sookyeong0505@gmail.com>
2024-08-13 15:50:50 +0900

    라우터 적용

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,47 +1,21 @@
 import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import Detail from "./routes/Detail";
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
 
 function MovieApp() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-    const getMovies = async() => {
-        // const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
-        // const json = await response.json();
-        const json = await (
-            await fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-        )).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-
-    useEffect(()=> {
-        getMovies();
-    }, []);
-
-    console.log(movies);
-
-    return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-            <div>
-                {movies.map( movie => (
-                    <Movie
-                        key={movie.id}
-                        coverImg={movie.medium_cover_image}
-                        title={movie.title}
-                        summary={movie.summary}
-                        genres={movie.genres}
-                    />
-                    ))}
-            </div>
-        )}
-        </div>
-    );
-
+    return <Router>
+        <Routes>
+            <Route path="/movie/:id" element={<Detail/>} />
+            <Route path="/" element={<Home />} />
+            
+        </Routes>
+    </Router>;
 }
 
 export default MovieApp;
